9617b5584c7c6fb744f6201f0f0b5115be0aab5a
CatIsNotFound <liaoxinkai0408@outlook.com>
2024-06-28 09:57:55 +0800

    20240628

--- .scripts/test.sh
+++
@@ -1,9 +0,0 @@
-# 判断 ~/apt 目录是否存在
-if [ ! -d ~/apt ]; then
-    mkdir ~/apt
-fi
-
-# 判断 ~/apt/history.log 文件是否存在
-if [ ! -f ~/apt/history.log ]; then
-    cp /var/log/apt/history.log ~/apt/history.log
-fi
